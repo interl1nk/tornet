0ffaf2afa3b1fb1415b412cef30be5ce55cc2035
Herman <hmurauyou@icloud.com>
2025-01-07 18:08:08 +0300

    SETUP: network fixed.

--- pkg/settings/software.sh
+++ pkg/settings/software.sh
@@ -13,14 +13,14 @@ function services() {
       echo "Using Linux: $OS"
 
       get_active_interface_linux
-      check_internet_connection
+      check_internet_connection || { echo "Exiting due to no internet."; exit 1; }
 
       tor_service_linux
   elif [[ "$OS" == "Darwin" ]]; then
       echo "$(set_color "green")✓$(set_color "*") Using MacOS: $OS"
 
       get_active_interface_macos
-      check_internet_connection
+      check_internet_connection || { echo "Exiting due to no internet."; exit 1; }
 
       tor_service_macos
   else
