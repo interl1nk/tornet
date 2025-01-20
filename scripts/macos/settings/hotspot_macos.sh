97c51e61c5d09cd5caee77dfdf1f133976748fd6
Herman <hmurauyou@icloud.com>
2025-01-20 22:44:27 +0300

    DEV: settings logs fixed.

--- scripts/macos/settings/hotspot_macos.sh
+++ scripts/macos/settings/hotspot_macos.sh
@@ -1,18 +1,16 @@
 #! /bin/bash
 
-source ./scripts/pkg/assets/colors.sh
-
 function hot_spot_macos() {
-    echo "$(set_color "purple")[•]$(set_color "*") Configuring MacOS network"
-    echo "$(set_color "gray")[•]$(set_color "*") Enter your password below:"
+    log INFO "Configuring MacOS network"
 
-    sudo networksetup -setsocksfirewallproxy "Wi-Fi" 127.0.0.1 9050
-    sudo networksetup -setsocksfirewallproxystate "Wi-Fi" on
+    networksetup -setsocksfirewallproxy "Wi-Fi" 127.0.0.1 9050
+    networksetup -setsocksfirewallproxystate "Wi-Fi" on
 
-    echo "$(set_color "green")[✓]$(set_color "*") MacOS network configured."
+    log OK "MacOS network configured"
 }
 
 function restore_settings() {
-    echo "$(set_color "gray")[•]$(set_color "*") Restoring original settings..."
-    sudo networksetup -setsocksfirewallproxystate "Wi-Fi" off
+    log INFO "Restoring original settings"
+    networksetup -setsocksfirewallproxystate "Wi-Fi" off
+    brew services stop tor
 }
--- scripts/macos/settings/tor_config.sh
+++ scripts/macos/settings/tor_config.sh
@@ -1,24 +1,22 @@
 #! /bin/bash
 
-source ./scripts/pkg/assets/colors.sh
-
 TOR_CONFIG_MACOS="/opt/homebrew/etc/tor/torrc"
 BRIDGE_FILE="bridge.txt"
 
 function verify_tor_config() {
-    echo "$(set_color "purple")[•]$(set_color "*") Verifying Tor configuration"
+    log INFO "Verifying Tor configuration"
 
     if [[ ! -f "$TOR_CONFIG_MACOS" ]]; then
-        echo "$(set_color "yellow")[ALERT]$(set_color "*"): Tor configuration file not found. Creating."
+        log ALERT "Tor configuration file not found. Creating"
         touch "$TOR_CONFIG_MACOS"
         echo "UseBridges 1" >> "$TOR_CONFIG_MACOS"
-        echo "$(set_color "green")[✓]$(set_color "*") Created new Tor configuration file with default settings."
+        log OK "Created new Tor configuration file with default settings"
     else
-        echo "$(set_color "purple")[•]$(set_color "*") Tor configuration file found: $TOR_CONFIG_MACOS"
+        log OK "Tor configuration file found: $TOR_CONFIG_MACOS."
     fi
 
     if [[ ! -f "$BRIDGE_FILE" ]]; then
-        echo "$(set_color "red")[ERROR]$(set_color "*"): Bridge file not found at $BRIDGE_FILE."
+        log ERROR "Bridge file not found at $BRIDGE_FILE."
         exit 1
     fi
 
@@ -40,14 +38,14 @@ function update_tor_config() {
     done < "$BRIDGE_FILE"
 
     if [[ ${#bridges[@]} -eq 0 ]]; then
-        echo "$(set_color "red")[ERROR]$(set_color "*"): Bridges file is empty or contains invalid data."
+        log ERROR "Bridges file is empty or contains invalid data."
         exit 1
     fi
 
     local new_bridge="${bridges[0]}"
 
     cp "$TOR_CONFIG_MACOS" "${TOR_CONFIG_MACOS}.bak"
-    echo "$(set_color "yellow")[•]$(set_color "*") Backup created: ${TOR_CONFIG_MACOS}.bak"
+    log INFO "Backup created: ${TOR_CONFIG_MACOS}.bak"
 
     if ! grep -q '^UseBridges 1' "$TOR_CONFIG_MACOS"; then
         echo 'UseBridges 1' | tee -a "$TOR_CONFIG_MACOS" > /dev/null
@@ -55,10 +53,10 @@ function update_tor_config() {
 
     sed -i '' '/^Bridge /d' "$TOR_CONFIG_MACOS"
     echo "Bridge $new_bridge" | tee -a "$TOR_CONFIG_MACOS" > /dev/null
-    echo "$(set_color "green")[✓]$(set_color "*") Bridge updated to: $new_bridge"
+    log OK "Bridge updated to: $new_bridge"
 
     sed -i '' '1d' "$BRIDGE_FILE"
-    echo "$(set_color "purple")[•]$(set_color "*") Used bridge removed from the bridge file."
+    log INFO "Used bridge removed from the bridge file"
 }
 
 update_tor_config
