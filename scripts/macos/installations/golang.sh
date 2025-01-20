645733fd8a2ed9eaa10fbbfcf1a2c01178a2cf17
Herman <hmurauyou@icloud.com>
2025-01-20 22:44:03 +0300

    DEV: installations logs fixed.

--- scripts/macos/installations/golang.sh
+++ scripts/macos/installations/golang.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
-source ./scripts/pkg/assets/colors.sh
-
 function golang_macos() {
-    echo "$(set_color "purple")[•]$(set_color "*") Checking Golang version"
+    log INFO "Checking Golang version"
 
     if ! command -v go &>/dev/null; then
-        echo "$(set_color "yellow")[ALERT]$(set_color "*"): Golang is not installed"
+        log ALERT "Golang is not installed"
 
-        echo "$(set_color "purple")[•]$(set_color "*") Installing Golang latest version"
+        log INFO "Installing Golang latest version"
         brew install go
 
         exit 1
@@ -16,6 +14,5 @@ function golang_macos() {
 
     local go_version
     go_version=$(go version 2>/dev/null | grep -oE 'go[0-9]+\.[0-9]+(\.[0-9]+)?' | sed 's/go//')
-    echo "$(set_color "gray")[•]$(set_color "*") Go version: $go_version"
-    echo "$(set_color "green")[✓]$(set_color "*") OK."
+    log INFO "Go version: $go_version"
 }
--- scripts/macos/installations/tor.sh
+++ scripts/macos/installations/tor.sh
@@ -1,18 +1,16 @@
 #! /bin/bash
 
-source ./scripts/pkg/assets/colors.sh
-
 function tor_service_macos() {
-    echo "$(set_color "purple")[•]$(set_color "*") Checking Tor status"
+    log INFO "Checking Tor status"
 
     if ! brew list --formula | grep -q "^tor$"; then
-        echo "$(set_color "yellow")[ALERT]$(set_color "*"): Tor is not installed"
-        echo "$(set_color "purple")[•]$(set_color "*") Installing Tor via Homebrew"
+        log ALERT "Tor is not installed"
+        log INFO "Installing Tor via Homebrew"
 
         if brew install tor &> /dev/null; then
-            echo "$(set_color "green")[✓]$(set_color "*") Tor has been installed successfully."
+            log OK "Tor has been installed successfully"
         else
-            echo "$(set_color "red")[ERROR]$(set_color "*"): Failed to install Tor. Please check your Homebrew setup."
+            log ERROR "Failed to install Tor. Please check your Homebrew setup."
             return 1
         fi
     fi
@@ -20,15 +18,15 @@ function tor_service_macos() {
     tor_status=$(brew services list | grep tor)
 
     if echo "$tor_status" | grep -q "started\|Active: active"; then
-        echo "$(set_color "green")[✓]$(set_color "*") Service Tor is running."
+        log OK "Service Tor is running"
     else
-        echo "$(set_color "yellow")[ALERT]$(set_color "*"): Tor is down"
-        echo "$(set_color "purple")[•]$(set_color "*") Starting Tor"
+        log ALERT "Tor is down"
+        log INFO "Starting Tor"
 
         if tor_output=$(brew services start tor 2>&1); then
-            echo "$(set_color "green")[✓]$(set_color "*") Service Tor is running."
+            log OK "Service Tor is running"
         else
-            echo "$(set_color "red")[ERROR]$(set_color "*"): Error starting Tor"
+            log ERROR "Error starting Tor."
             echo "$tor_output"
             return 1
         fi
