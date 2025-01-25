deca4484c27cc1158bb37ba1bce814dc391c7507
Herman <hmurauyou@icloud.com>
2025-01-25 21:36:15 +0300

    DEV: main updated.

--- main.sh
+++ main.sh
@@ -1,40 +1,17 @@
 #! /bin/bash
 
-source ./scripts/linux/software/software.sh
-source ./scripts/linux/services/services.sh
+source ./scripts/macos/macos.sh
+source ./scripts/shared/logging/freelog.sh
 
-source ./scripts/macos/software/software.sh
-source ./scripts/macos/services/services.sh
-source ./scripts/macos/settings/proxy_config.sh
-source hotspot.sh
-source ./scripts/macos/settings/bridge.sh
-source ./scripts/macos/settings/tor_config.sh
-
-source ./scripts/macos/settings/sigint.sh
-
-source ./scripts/pkg/logging/freelog.sh
-
-trap handle_sigint SIGINT
-
-function setup() {
+function main() {
     echo "@Freeman"
 
     OS=$(uname)
 
     if [[ "$OS" == "Linux" ]]; then
-        software_linux
-        services_linux
+        echo "Downloading..."
     elif [[ "$OS" == "Darwin" ]]; then
-        software_macos
-        services_macos
-        change_proxy_config
-        check_tor_bridges
-        get_bridge
-        create_tor_config
-        update_tor_config
-        verify_tor_config
-        test_tor_bridge
-        hot_spot_macos
+        macos_setup
     else
         log ERROR "Unsupported OS: $OS"
         return 1
@@ -45,4 +22,4 @@ function setup() {
     done
 }
 
-setup
+main
